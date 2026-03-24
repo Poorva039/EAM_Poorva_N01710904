0f9c3efa8eb587da7a35643df4ef24ad51ce3e66
Poorva Patel <118352102+Poorva039@users.noreply.github.com>
2026-03-23 20:54:00 -0400

    Add files via upload

---
+++ rabbitmq-message-microservices/Week_10_ConsumerService/src/main/java/com/example/week10/RabbitMQConfig.java
@@ -0,0 +1,22 @@
+package com.example.week10;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    public static final String QUEUE_NAME = "exampleQueue";
+
+    @Bean
+    public Queue queue() {
+        return new Queue(QUEUE_NAME, false);
+    }
+
+    @RabbitListener(queues = QUEUE_NAME)
+    public void receive(String message) {
+        System.out.println("Received: " + message);
+    }
+}
---
+++ rabbitmq-message-microservices/Week_10_ConsumerService/src/main/java/com/example/week10/Week10ConsumerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.week10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week10ConsumerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Week10ConsumerServiceApplication.class, args);
+	}
+
+}
